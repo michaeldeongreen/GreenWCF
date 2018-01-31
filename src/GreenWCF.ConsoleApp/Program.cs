3849e1566fd33c6e144e522799db7c2421b3abb0
michaeldeongreen <michaeldeongreen@gmail.com>
2018-01-31 10:15:05 -0600

    *Initial check in

---
+++ src/GreenWCF.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GreenWCF.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GreenServiceProxy.GreenServiceClient client = new GreenServiceProxy.GreenServiceClient();
+            string value = client.Get(39);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/GreenWCF.Services/SomeService.cs
@@ -0,0 +1,17 @@
+﻿using GreenWCF.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenWCF.Services
+{
+    public class SomeService : ISomeService
+    {
+        public string Print()
+        {
+            return "Printing....";
+        }
+    }
+}
